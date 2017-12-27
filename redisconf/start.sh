ea1eefbe5563e62702c04e009f0bd81f67f4688b
wenchao.meng <oytmfc@gmail.com>
2017-12-27 17:39:11 +0800

    init redis

---
+++ redisconf/start.sh
@@ -0,0 +1,39 @@
+. ./fun.sh
+
+count=$1
+if [ -z $count ];then
+		count=2
+fi
+
+
+echo "redis count:"$count
+port=6379
+
+function startRedis(){
+
+	currentPort=$1
+	echo ========start redis  $currentPort=================
+
+	rm -rf  $currentPort
+	makedir $currentPort
+	cp fun.sh template/redis.sh template/master.conf $currentPort
+
+	cd $currentPort
+	sh redis.sh
+	cd ..
+}
+
+initport=$port
+startRedis $port
+
+for (( i=1;i<$count;i++ ))
+do
+		port=$(( $port + 100 ))
+		startRedis $port
+		echo "make redis slaveof $initport"
+		redis 127.0.0.1 $port "slaveof 127.0.0.1 $initport"
+done
+
+
+
+ps -ef | grep '[r]edis-server'
---
+++ redisconf/template/redis.sh
@@ -0,0 +1,43 @@
+DIR=`dirname $0`
+FULL_DIR=`pwd`/$DIR
+echo $DIR
+
+CONFIG=$DIR/master.conf
+DATA=$DIR/data
+LOG=$DIR/log
+
+. $DIR/fun.sh
+
+PORT=`getPort $FULL_DIR`
+
+makedir $DATA
+makedir $LOG
+
+if [ -f $DIR/slave.conf ]
+then
+	CONFIG=$DIR/slave.conf
+fi
+
+if [ -n "$PORT"  ];then
+    echo "port from dir:$PORT"
+    sed -i "s/port.*/port $PORT/" $DIR/*.conf
+fi
+
+PORT=`cat $CONFIG | grep port | awk '{print $2}'`
+
+echo "Using config file:"$CONFIG $PORT
+
+for pid in `ps -ef | grep 'redis-serve[r]' | grep "$PORT" | awk '{print $2}'`; do
+	echo "killing" $pid
+	kill -9 $pid
+done
+
+sleep 1
+
+
+DATA_DIR=$FULL_DIR"/data"
+sed -i  "s#dir.*#dir $DATA_DIR#"   $DIR/*.conf
+nohup redis-server  $CONFIG  > $LOG/master.log 2>&1 &
+sleep 2
+setProtectedMode 127.0.0.1 $PORT
+
